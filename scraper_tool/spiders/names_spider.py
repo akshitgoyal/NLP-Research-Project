38fe775f05c7824566fc74a1c6e9c6c9f35f12fc
Akshit Goyal <goyalakshit.ag@gmail.com>
2020-06-12 13:07:46 +0530

    Crawls past year coonferences as well

--- scraper_tool/spiders/names_spider.py
+++ scraper_tool/spiders/names_spider.py
@@ -8,10 +8,27 @@ class namesSpider(scrapy.Spider):
     allowed_domains = ["bigdataieee.org"]
 
     start_urls = [
-        "http://bigdataieee.org/BigData2016/"
+        "http://bigdataieee.org/"
     ]
 
     def parse(self, response):
+        """
+        This is the main parse method. It will automatically called when we
+        make a call to this spyder from command line.
+        This method extracts all the conferences from past year whose links are
+        available on the conference home page.
+        """
+        conference_links = response.xpath("//nav/ul/li/a")
+        for conference in conference_links:
+            link = conference.xpath("@href").extract_first()
+            if not(link == "index.html" or "2014" in link or "2013" in link):
+                yield scrapy.Request(link, callback=self.parse_conference)
+
+    def parse_conference(self, response):
+        """
+        This method is used to parse each conference. It makes calls to different
+        parser methods depending on the type of information we want.
+        """
         links = response.xpath("//a")
         for link in links:
             page_name = link.xpath("text()").extract_first()
@@ -25,16 +42,15 @@ class namesSpider(scrapy.Spider):
                 url = response.urljoin(link.xpath("@href").extract_first())
                 yield scrapy.Request(url, callback=self.parse_keynote_speakers)
 
-
-
-
-
     def parse_committee_members(self, response):
+        """
+        This method is used for parsing the names of the people from committee
+        members page.
+        """
         names = []
         special = False
         for sel in response.xpath('//span'):
             name = sel.xpath('text()').extract()
-            print(name)
             for n1 in name:
                 if ':' in n1:
                     temp = n1.split(':')[0].strip()
@@ -46,8 +62,8 @@ class namesSpider(scrapy.Spider):
                     names.append(n1.split(',')[0].strip())
             if special:
                 break
-
-        # Some tags have <strong> in it which will make it difficult so we will treat it as a special cas.
+        # Due to different structure amongst different pages, I had to add a
+        # special case to extract all the information.
         if special:
             for sel in response.xpath('//span/strong'):
                 name = sel.xpath('text()').extract()
@@ -56,41 +72,50 @@ class namesSpider(scrapy.Spider):
                         names.append(n1.split(':')[0].strip())
                     else:
                         names.append(n1.split(',')[0].strip())
-        if os.path.isfile('names.csv'):
+
+        file_name = response.xpath("//title/text()").extract_first() + ".csv"
+        if os.path.isfile(file_name):
             mode = 'a+'
         else:
-            mode = 'w'
-        with open('names.csv', mode) as file:
+            mode = 'w+'
+        with open(file_name, mode) as file:
             writer = csv.writer(file)
-            writer.writerow(['Names', 'Organization', 'Location'])
+            # writer.writerow(['Names', 'Organization', 'Location'])
+            writer.writerow(['Names'])
             for n in names:
                 writer.writerow([n])
 
     def parse_program_committee(self, response):
+        """
+        This method is used for parsing the names of the people from program
+        committee page.
+        """
         details = response.xpath("//tr/th")
-        if os.path.isfile('names.csv'):
+        file_name = response.xpath("//title/text()").extract_first() + ".csv"
+        if os.path.isfile(file_name):
             mode = 'a+'
         else:
-            mode = 'w'
+            mode = 'w+'
+
         for i in range(0,len(details),3):
             name = details[i].xpath('text()').extract_first()
-            with open('names.csv', mode) as file:
+            with open(file_name, mode) as file:
                 write = csv.writer(file)
                 write.writerow([name])
-            # print(name)
-
 
     def parse_keynote_speakers(self, response):
-
+        """
+        This method is used for parsing the names of the people from keynote
+         speakers page.
+        """
         speaker_info = response.xpath("//tr/td/div/p/strong")
-        if os.path.isfile('names.csv'):
+        file_name = response.xpath("//title/text()").extract_first() + ".csv"
+        if os.path.isfile(file_name):
             mode = 'a+'
         else:
-            mode = 'w'
+            mode = 'w+'
         for i in range(len(speaker_info)):
             name = speaker_info[i].xpath('text()').extract_first()
-            with open('names.csv', mode) as file:
+            with open(file_name, mode) as file:
                 write = csv.writer(file)
                 write.writerow([name])
-
-
