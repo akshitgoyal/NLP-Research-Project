22ab76a0bfbd358a023c04260b05ac57b2270f9d
Akshit Goyal <goyalakshit.ag@gmail.com>
2020-07-01 04:29:31 +0530

    Cleaned the code

--- scraper_tool/spiders/names_spider.py
+++ scraper_tool/spiders/names_spider.py
@@ -1,9 +1,13 @@
 import scrapy
 import csv
-import os
 
 
 class namesSpider(scrapy.Spider):
+    """
+    Please visit the GitHub documentation for further details on usage and
+    improvements that can be done in the next iteration.
+    """
+
     name = "people"
     allowed_domains = ["bigdataieee.org"]
 
@@ -21,7 +25,8 @@ class namesSpider(scrapy.Spider):
         conference_links = response.xpath("//nav/ul/li/a")
         for conference in conference_links:
             link = conference.xpath("@href").extract_first()
-            if not(link == "index.html" or "2014" in link or "2013" in link):
+            if not(link == "index.html" or
+                   any(year in link for year in ["2013", "2014", "2015"])):
                 yield scrapy.Request(link, callback=self.parse_conference)
 
     def parse_conference(self, response):
@@ -48,74 +53,74 @@ class namesSpider(scrapy.Spider):
         members page.
         """
         names = []
-        special = False
-        for sel in response.xpath('//span'):
+        # We need to determine the year of conf from title as 2016 has
+        # different structure for this section.
+        title = response.xpath("//title/text()").extract_first()
+        if any(year in title for year in ["2016", "2017", "2018"]):
+            html_tag = '//span/strong'
+        else:
+            html_tag = '//span'
+
+        # Data Scraping
+        for sel in response.xpath(html_tag):
             name = sel.xpath('text()').extract()
-            for n1 in name:
+            for n1 in name:  # Format varies in different years by ":" or ","
                 if ':' in n1:
-                    temp = n1.split(':')[0].strip()
-                    if temp == '':
-                        special = True
-                        break
-                    names.append(temp)
+                    names.append(n1.split(':')[0].strip())
                 else:
                     names.append(n1.split(',')[0].strip())
-            if special:
-                break
-        # Due to different structure amongst different pages, I had to add a
-        # special case to extract all the information.
-        if special:
-            for sel in response.xpath('//span/strong'):
-                name = sel.xpath('text()').extract()
-                for n1 in name:
-                    if ':' in n1:
-                        names.append(n1.split(':')[0].strip())
-                    else:
-                        names.append(n1.split(',')[0].strip())
 
+        # File Writing
         file_name = response.xpath("//title/text()").extract_first() + ".csv"
-        if os.path.isfile(file_name):
-            mode = 'a+'
-        else:
-            mode = 'w+'
-        with open(file_name, mode) as file:
-            writer = csv.writer(file)
-            # writer.writerow(['Names', 'Organization', 'Location'])
-            writer.writerow(['Names'])
-            for n in names:
-                writer.writerow([n])
+        data_cat = "Organization Committee Members"
+        write_data_to_file(file_name, names, data_cat)
 
     def parse_program_committee(self, response):
         """
         This method is used for parsing the names of the people from program
         committee page.
         """
+        # Data Scraping
         details = response.xpath("//tr/th")
         file_name = response.xpath("//title/text()").extract_first() + ".csv"
-        if os.path.isfile(file_name):
-            mode = 'a+'
-        else:
-            mode = 'w+'
-
+        names = []
         for i in range(0,len(details),3):
-            name = details[i].xpath('text()').extract_first()
-            with open(file_name, mode) as file:
-                write = csv.writer(file)
-                write.writerow([name])
+            names.append(details[i].xpath('text()').extract_first())
+
+        # File Writing
+        data_cat = "Program Committee Members"
+        write_data_to_file(file_name, names, data_cat)
 
     def parse_keynote_speakers(self, response):
         """
         This method is used for parsing the names of the people from keynote
-         speakers page.
+        speakers page.
         """
+        # Data Scraping
         speaker_info = response.xpath("//tr/td/div/p/strong")
         file_name = response.xpath("//title/text()").extract_first() + ".csv"
-        if os.path.isfile(file_name):
-            mode = 'a+'
-        else:
-            mode = 'w+'
+        names = []
         for i in range(len(speaker_info)):
-            name = speaker_info[i].xpath('text()').extract_first()
-            with open(file_name, mode) as file:
-                write = csv.writer(file)
-                write.writerow([name])
+            names.append(speaker_info[i].xpath('text()').extract_first())
+
+        # File Writing
+        data_cat = "Keynote Speakers"
+        write_data_to_file(file_name,names, data_cat)
+
+
+def write_data_to_file(file_name, names, data_cat):
+    """
+    This function writes the data to the file taking into the account of different
+    years and also divides the csv file into sections.
+
+    NOTE: The sections in csv file are separated by adding "" in the row after
+    each section.
+    """
+    writer = csv.writer(open(file_name, "a+"))
+    writer.writerow([data_cat])
+    for n in names:
+        if n.strip().lower() != "name":
+            writer.writerow([n])
+    writer.writerow([""])  # This signifies that the category ended.
+
+
